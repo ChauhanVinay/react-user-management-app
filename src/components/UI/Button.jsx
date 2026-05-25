5c6838584a81fb62fb40bedd41ed4df7322df00f
ChauhanVinay <vc0627344@gmail.com>
2026-05-25 16:32:28 +0530

    Adding Reusable Button

---
+++ src/components/UI/Button.jsx
@@ -0,0 +1,11 @@
+import './Button.css';
+
+const Button = (props) => {
+    return (
+        <button className='button' type={props.type || "button"}>
+            {props.children}
+        </button>
+    );
+};
+
+export default Button;
--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -1,5 +1,6 @@
 import './AddUser.css';
 import Card from '../UI/Card';
+import Button from '../UI/Button';
 
 const AddUser = () => {
   const addUserHandler = (event) => {
@@ -15,7 +16,7 @@ const AddUser = () => {
                 <label htmlFor="age">Age:</label>
                 <input type="number" id="age" />
 
-            <button type="submit">Add User</button>
+            <Button type="submit">Add User</Button>
         </form>
         </Card>
     );
